f5dd0e76c0b5a24e4a7e741b7b02784abf96fd4e
jerryjohnsoffl <jerryjohnsoffl@gmail.com>
2025-10-04 19:13:15 +0530

    background added

--- frontend/src/pages/WeatherPage.jsx
+++ frontend/src/pages/WeatherPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import ChanceOfRain from '../components/ChanceOfRain'
 
 const WeatherPage = () => {
   return (
-    <div className="w-full min-h-screen bg-gradient-to-b from-[#52C7E8] to-[#96D9EC] flex justify-center items-center">
-        <div className="container flex justify-center items-center w-4/5 h-4/5 bg-white">
-            Hello
+    <div className="w-full min-h-screen bg-gradient-to-b from-[#52C7E8] to-[#96D9EC] pt-12">
+        <div className="h-full w-full flex flex-col justify-center items-center">
+                <div className="container flex justify-center rounded-4xl items-center w-3/4 h-[548px] bg-sh-white">
+            
+            </div>
+            <button className="my-5 py-3 px-10 h-14 w-2xs font-medium text-t-black rounded-full bg-sh-white">View past 7 days</button>
         </div>
     </div>
   )
