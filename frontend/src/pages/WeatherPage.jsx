21d2c4fb5c422394a74ef732dcdf5cbacf39233b
jerryjohnsoffl <jerryjohnsoffl@gmail.com>
2025-10-04 19:35:43 +0530

    background added

--- frontend/src/pages/WeatherPage.jsx
+++ frontend/src/pages/WeatherPage.jsx
@@ -5,10 +5,13 @@ const WeatherPage = () => {
   return (
     <div className="w-full min-h-screen bg-gradient-to-b from-[#52C7E8] to-[#96D9EC] pt-12">
         <div className="h-full w-full flex flex-col justify-center items-center">
-                <div className="container flex justify-center rounded-4xl items-center w-3/4 h-[548px] bg-sh-white">
-            
+            <div className="container w-11/12 h-44 bg-sh-white mb-8 rounded-3xl sm:hidden">
+                {/* Add location and details for phone ratio */}
             </div>
-            <button className="my-5 py-3 px-10 h-14 w-2xs font-medium text-t-black rounded-full bg-sh-white">View past 7 days</button>
+            <div className="container flex justify-center rounded-3xl md:rounded-4xl items-center w-11/12 sm:w-3/4 h-[548px] bg-sh-white">
+                {/* Both phone and desktop */}
+            </div>
+            <button className="my-4 py-3 px-10 h-14 w-2xs font-medium text-t-black rounded-full bg-sh-white">View past 7 days</button>
         </div>
     </div>
   )
