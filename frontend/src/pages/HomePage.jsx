d9799dffa16a57e84f88fb5ef8f7265a3ba83646
jerryjohnsoffl <jerryjohnsoffl@gmail.com>
2025-10-04 23:11:33 +0530

    components added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div className="w-full h-full bg-gradient-to-b from-[#52C7E8] to-[#96D9EC]">
-        Homepage
+    <div className="w-full min-h-screen bg-gradient-to-b from-[#52C7E8] to-[#96D9EC]">
+        <div className="h-full w-full flex flex-col justify-center items-center">
+            <div className="h-[467px] w-3/4 bg-sh-white"></div>
+        </div>
     </div>
   )
 }
