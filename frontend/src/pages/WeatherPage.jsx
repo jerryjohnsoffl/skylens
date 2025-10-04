f13c9f33e90926051f01fcd9ad3576c2c33ee89c
jerryjohnsoffl <jerryjohnsoffl@gmail.com>
2025-10-04 21:06:15 +0530

    section added

--- frontend/src/pages/WeatherPage.jsx
+++ frontend/src/pages/WeatherPage.jsx
@@ -5,31 +5,31 @@ const WeatherPage = () => {
   return (
     <div className="w-full min-h-screen bg-gradient-to-b from-[#52C7E8] to-[#96D9EC] pt-12">
         <div className="h-full w-full flex flex-col justify-center items-center">
-            <div className="container w-11/12 h-44 bg-sh-white mb-8 rounded-3xl sm:hidden">
+            <div className="container w-11/12 h-44 bg-sh-white mb-8 rounded-3xl md:hidden">
                 {/* Add location and details for phone ratio */}
                 {/* Start here and remove these comments after completion*/}
             </div>
-            <div className="container flex flex-col sm:flex-row justify-center rounded-3xl md:rounded-4xl items-center w-11/12 sm:w-3/4 h-[548px] bg-sh-white">
+            <div className="container flex flex-col md:flex-row justify-center rounded-3xl md:rounded-4xl items-center w-11/12 sm:w-3/4 h-[548px] bg-sh-white">
                 {/* Both phone and desktop */}
-                <div className="w-full h-auto">
+                <div className="w-full h-auto md:h-full md:w-3/5">
                     {/* highlights and loc */}
-                    <div className="w-full h-auto">
+                    <div className="w-full h-auto md:h-1/4">
                         {/* best day, loc */}
-                        <div className="w-full h-auto hidden sm:flex">
+                        <div className="w-full h-auto hidden md:flex">
                             {/* loc */}
                         </div>
                         <div className="w-full h-36">
                             {/* best day */}
                         </div>
                     </div>
-                    <div className="w-full">
+                    <div className="w-full h-auto md:h-3/4">
                         {/* highlights and data */}
-                        <div className="w-full flex flex-col items-center">
+                        <div className="w-full md:w-1/2 flex flex-col md:flex-row items-center">
                             {/* highlights */}
-                            <div className="w-full h-56">
+                            <div className="w-full h-3/4">
                                 {/* uv index */}
                             </div>
-                            <button className="sm:hidden">Show more</button>
+                            <button className="md:hidden">Show more</button>
                             {/* till here, and call me */}
                         </div>
                         <div className="w-full">
@@ -37,7 +37,7 @@ const WeatherPage = () => {
                         </div>
                     </div>
                 </div>
-                <div className="w-full flex-col">
+                <div className="w-full flex-col md:h-full md:w-2/5">
                     {/* graph and suggestions */}
                     <div className="">
                         {/* graph */}
