c3f1b226ca166c820739657c2ed1a56843fe2dab
jerryjohnsoffl <jerryjohnsoffl@gmail.com>
2025-10-05 10:20:20 +0530

    Homepage added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -7,6 +7,15 @@ import HomePage from './pages/HomePage'
 import WeatherPage from './pages/WeatherPage'
 
 function App() {
+  useEffect(()=> {
+    const fetchData = async ()=> {
+      try{
+
+      } catch(err) {
+        console.error("Failed to fetch the error", err)
+      }
+    }
+  },[])
 
   return (
     <>
